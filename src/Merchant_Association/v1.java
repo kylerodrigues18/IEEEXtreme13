e8c6e66adb8687ee3bd397a0d0c3e1f9c315e5a3
Kyle Rodrigues <kylerodrigues18@gmail.com>
2019-10-19 00:25:28 -0500

    Updated Merchant to have all the code that should make it to work, but it still does not work :(

--- src/Merchant_Association/v1.java
+++ src/Merchant_Association/v1.java
@@ -10,7 +10,7 @@ public class v1 {
         int numTowns = in.nextInt();
         int[] townPrices = new int[numTowns];
         HashMap<Integer, ArrayList<Integer>> routes = new HashMap<>();
-        int[] merchantsPosition;
+        ArrayList<Integer> merchantsPosition;
 
         //init prices array
         for(int i = 0; i < numTowns; i++) {
@@ -30,13 +30,60 @@ public class v1 {
             routes.get(to).add(from);
         }
 
-        //init merchant position array
+        //init merchant position ArrayList
         int merchantNumber = in.nextInt();
-        merchantsPosition = new int[merchantNumber];
+        merchantsPosition = new ArrayList<>();
         for(int i = 0; i < merchantNumber; i++) {
-            merchantsPosition[i] = in.nextInt();
+            merchantsPosition.add(in.nextInt());
         }
 
-        //finished receiving necessary data, now time to get the answer #killme
+        int totalCost = 0;
+        int merchant = 0;
+        int purchaseLocation = 0;
+        int sellingLocation = 0;
+        int cost = 0;
+        for (int m = 0; m < merchantNumber; m++) {
+
+            //finished receiving necessary data, now time to get the answer #killme
+            for (int i = 0; i < merchantNumber; i++) {
+                //Get and store the merchant initial location
+                int merchantInitLocation = merchantsPosition.get(i);
+
+                //Using the initial merchant location, find the routes to travel from it
+                ArrayList<Integer> purchaseLoc = routes.get(merchantInitLocation);
+
+                for (int purchase = 0; purchase < purchaseLoc.size(); purchase++) {
+                    //Temporarily move the merchant to the purchase location
+                    merchantsPosition.set(merchantInitLocation, purchaseLoc.get(purchase));
+
+                    //if the purchase location is found in the merchant position, then do nothing since a merchant is located in that city
+                    if (merchantsPosition.indexOf(purchaseLoc.get(purchase)) == -1) {
+                        //Get the price to buy the goods
+                        int tempCost = townPrices[purchaseLoc.get(purchase)];
+
+                        //Create an ArrayList of selling locations from the purchase location
+                        ArrayList<Integer> sellingLoc = routes.get(purchaseLoc.get(purchase));
+
+                        for (int sellingPlace : sellingLoc) {
+                            //If the selling location is found in the merchant position, then do nothing since a merchant is located in that city
+                            if (merchantsPosition.indexOf(sellingPlace) != -1) {
+                                int compareCost = townPrices[sellingPlace] - tempCost;
+                                if (compareCost > cost) {
+                                    cost = compareCost;
+                                    merchant = i;
+                                    purchaseLocation = purchase;
+                                    sellingLocation = sellingPlace;
+                                }
+                            }
+                        }
+                    }
+                }
+                merchantsPosition.set(i, merchantInitLocation);
+            }
+            merchantsPosition.remove(merchant);
+            totalCost += cost;
+            cost = 0;
+        }
+        System.out.println(totalCost);
     }
 }
