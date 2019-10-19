47e0c95b5bd85ff53fa403868a9f78be907d066e
irondeau <isaiahrondeau@gmail.com>
2019-10-18 23:15:13 -0500

    Add Unicornosaurus

---
+++ src/Unicornosaurus/v1.java
@@ -0,0 +1,11 @@
+package Unicornosaurus;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class v1 {
+    public static void main(String[] args) {
+
+    }
+}
