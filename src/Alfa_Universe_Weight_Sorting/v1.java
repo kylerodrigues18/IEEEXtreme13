ce36611889df67227fa4924cc2fa15fc72913f89
irondeau <isaiahrondeau@gmail.com>
2019-10-19 04:05:59 -0500

    Give up

---
+++ src/Alfa_Universe_Weight_Sorting/v1.java
@@ -0,0 +1,12 @@
+package Alfa_Universe_Weight_Sorting;
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+public class v1 {
+    
+    public static void main(String[] args) {
+
+    }
+}
